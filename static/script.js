0d790563b029130ba9790b34015fe9b5121e527f
Fahim Ahmed Rabbi <rabbi35-946@diu.edu.bd>
2025-07-30 22:24:31 +0600

    Update website with new routine and committee images

--- static/script.js
+++ static/script.js
@@ -7,3 +7,22 @@ function openLightbox(src) {
 function closeLightbox() {
   document.getElementById('lightbox').style.display = 'none';
 }
+
+const box = document.querySelector("#box");
+let i = 0;
+
+// Text
+let text = `Welcome to Palashbaria Secondary School! We are dedicated to providing quality education and fostering a supportive learning environment. Explore our website to learn more about our programs, faculty, and community activities.`;
+
+// Autotyping function specified for text variable
+const autoType = ()=>{
+    text.split("");
+    box.innerHTML+=text[i];
+    if(i == (text.length-1)){
+        clearInterval(m);
+    }
+    i++;
+}
+
+// Setting Intervals
+const m = setInterval(autoType,100);

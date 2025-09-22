c60de0361f71e4af1555aebaa13f58567c92786b
rabby946 <112797765+rabby946@users.noreply.github.com>
2025-09-22 14:53:30 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,16 +10,7 @@ def admin_required(view):
             flash("Login required", "error")
             return redirect(url_for('admin.login'))
         return view(*args, **kwargs)
-    return wrapped
+    return wrapped 
+   
 
-def upload_to_imgbb(file):
-    """Upload file to ImageBB and return URL"""
-    encoded_image = base64.b64encode(file.read()).decode("utf-8")
-    response = requests.post(
-        "https://api.imgbb.com/1/upload",
-        data={"key": Config.IMGBB_API_KEY, "image": encoded_image}
-    )
-    if response.status_code == 200:
-        return response.json()['data']['url']
-    else:
-        raise ValueError(f"Image upload failed: {response.text}")
+asasease
